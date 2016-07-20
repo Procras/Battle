d9c241357707f7ff2dcd2811e4d60a7492d5457e
Giancarlo <gfabrizi@gmail.com>
2016-07-20 13:57:21 +0200

    exercise21

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    sign_in_and_play
+    expect(page).to have_content 'Jess vs. Sam'
+  end
+end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,14 +1,10 @@
+require 'spec_helper'
 
-feature "See hit points" do
-  scenario "see player 2 hit points" do
-    player_1 = "Mannie"
-    player_2 = "Giancarlo"
 
-    visit "/"
-    fill_in :player_1, with: player_1
-    fill_in :player_2, with: player_2
-    click_button "Enter Battle"
-
-    expect(page).to have_text("#{player_2}: 100 Hit Points")
+feature 'hit points' do
+  scenario 'viewing hit point count' do
+    sign_in_and_play
+    expect(page).to have_content 'Sam has 100 hit points'
   end
+
 end
